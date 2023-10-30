7abdf10cd236786a8817c9330b325f8cac81d65e
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-10-30 06:36:51 +0200

    done with sync, now with PRs

--- workFlow.ts
+++ workFlow.ts
@@ -1,5 +1,30 @@
 /*
-1- create a new branch for the new feature
-  - git checkout -b EWMT5-fature-cool-feature dev
+1- Pull the latest changes from the remote "dev" into your local "dev" branch:
+   - git pull origin dev
+
+2- Create a new feature branch based on "dev":
+   - git checkout -b EWMT5-feature-cool-feature dev
+
+3- Make your changes and commit them with descriptive commit messages:
+   - git add .
+   - git commit -m "EWMT5: Implement cool feature"
+
+4- Pull the remote "dev" branch again to ensure your "dev" branch is up to date. Resolve any conflicts if necessary:
+   - git pull origin dev
+   - (Resolve conflicts if any)
+   - git add .
+   - git commit -m "EWMT5: Resolve conflicts"
+
+5- Push your feature branch to the remote repository:
+   - git push origin EWMT5-feature-cool-feature
+
+6- If your changes are accepted, merge your branch into "dev" (after pulling the latest changes from "dev"):
+   - git checkout dev
+   - git pull origin dev
+   - git merge EWMT5-feature-cool-feature
+   - git push origin dev
+
+
+
 
 */
